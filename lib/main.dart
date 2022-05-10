08efa3c22cba6e40ae1adbabedb8a724415ce6d7
Parikshit Pahare <p1pahare@techie.com>
2022-05-10 11:19:42 +0530

    added launcher icons in all platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:anime_themes_player/views/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,41 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '\$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      home: const SplashPage(),
     );
   }
 }
