12dfdadafe98be53d92da9f19e36927b69885429
Parikshit Pahare <p1pahare@techie.com>
2022-05-19 22:16:48 +0530

    fixed empty cover

--- lib/models/audio_entry.dart
+++ lib/models/audio_entry.dart
@@ -11,7 +11,8 @@ class AudioEntry {
   late final String album;
   late final String title;
   late final String url;
-  Uri art = Uri.file('lib/assets/cover.jpg');
+  Uri art = Uri.parse(
+      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj5REg76Q_U_rt-GxBXbi3C-76wOIKdcuGwl1PMOBoMkpdNva48wiHny2t2tCH2lFeh4GNCNrLlcXrx3mHH_V0vSwSPN9zG2iyUmmvuZbgbtjLsZ1FO6mOhmfuqUD54Uti8ieIXdTPi3ZbZDJzJxrv70pRah8g_rzLlb49JSWuUGfB2I7DdCTgiCdh9_A/s320/cover.jpg');
 
   AudioEntry.fromJson(Map<String, dynamic> json) {
     album = json['album'];
